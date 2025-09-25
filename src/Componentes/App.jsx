ca5eb4b27529b807a8a59e158c39a296a5d6e085
Fitzroyfang <Calvinofede@gmail.com>
2025-09-25 17:12:20 -0300

    comm2

--- src/Componentes/App.jsx
+++ src/Componentes/App.jsx
@@ -18,12 +18,12 @@ const App = () => {
     });
 
     const urlIP = import.meta.env.REACT_APP__IPSQL;
-/*
+
     const UrlTipoConfig = "/ConfiguracionEP"
-    const UrlTelas = "/TelasEP"*/
-  const UrlTipoConfig = "http://200.40.89.254:8086/Conf"
+    const UrlTelas = "/TelasEP"
+  /*const UrlTipoConfig = "http://200.40.89.254:8086/Conf"
     const UrlTelas = "http://200.40.89.254:8086/Telas"
-   
+   */
     const fetchRollerConf = async () => {
       try {
         const res = await fetch(UrlTipoConfig);
