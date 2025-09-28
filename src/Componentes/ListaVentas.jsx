58f6cfcd63e25e5bea0c7102a5e339309d19c694
FedeCalvino <calvinofede@gmail.com>
2025-09-28 20:12:28 -0300

    color

--- src/Componentes/ListaVentas.jsx
+++ src/Componentes/ListaVentas.jsx
@@ -31,7 +31,13 @@ export const ListaVentas = ({ ventas, onSelectVenta, selectedVentaId }) => (
       >
         <Row className="align-items-center">
           <Col className="col-8">
-          <div style={{ fontSize: "30px" }} className="fw-bold">
+          <div 
+            style={{ 
+              fontSize: "30px", 
+              color: Ven.estadoCorteTela === "TODOS TERMINADOS" ? "green" : "black" 
+            }} 
+            className="fw-bold"
+          >
             {Ven.obra.cliente?.nombre}
           </div>
           <div className="text-muted">{Ven.obra.nombre && Ven.obra.nombre}</div>
